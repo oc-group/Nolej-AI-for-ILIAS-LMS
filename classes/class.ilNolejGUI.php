047ae3e635fca47d4146af1a0da558792b19948f
Vincenzo Padula <padvincenzo@gmail.com>
2023-05-17 12:27:47 +0200

    Delete order table

--- classes/class.ilNolejGUI.php
+++ classes/class.ilNolejGUI.php
@@ -1,6 +1,5 @@
 <?php
 include_once("./Customizing/global/plugins/Services/Repository/RepositoryObject/Nolej/classes/class.ilNolejConfig.php");
-require_once("./Customizing/global/plugins/Services/Repository/RepositoryObject/Nolej/classes/class.ilNolejOrderTableGUI.php");
 
 /**
  * Plugin configuration class
