ef961119bff1b954a1e4c29e7d47a4ce9e5f1d51
Vincenzo Padula <padvincenzo@gmail.com>
2023-05-31 10:04:23 +0200

    Use RTE

--- classes/class.ilNolejActivityManagementGUI.php
+++ classes/class.ilNolejActivityManagementGUI.php
@@ -232,6 +232,7 @@ class ilNolejActivityManagementGUI
 				$txt = new ilTextAreaInputGUI($this->plugin->txt("prop_" . self::PROP_M_TEXTAREA), self::PROP_M_TEXTAREA);
 				$txt->setInfo($this->plugin->txt("prop_" . self::PROP_M_TEXTAREA . "_info"));
 				$txt->setUseRte(true);
+				$txt->setRteTagSet('mini');
 				$txt->setRequired(true);
 				$opt->addSubItem($txt);
 
