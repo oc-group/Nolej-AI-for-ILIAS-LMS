bbdf741ff379251ddfee12c021c32ba3947d51da
Vincenzo Padula <padvincenzo@gmail.com>
2023-06-04 21:13:12 +0200

    Notification fix

--- classes/GlobalScreen/NolejNotificationProvider.php
+++ classes/GlobalScreen/NolejNotificationProvider.php
@@ -7,10 +7,10 @@ namespace ILIAS\Nolej\Provider;
 // require_once("./src/GlobalScreen/Scope/Notification/Provider/AbstractNotificationPluginProvider.php");
 
 use ILIAS\GlobalScreen\Identification\IdentificationInterface;
-// use ILIAS\GlobalScreen\Scope\Notification\Provider\AbstractNotificationProvider;
-// use ILIAS\GlobalScreen\Scope\Notification\Provider\NotificationProvider;
 use ILIAS\GlobalScreen\Scope\Notification\Provider\AbstractNotificationPluginProvider;
 use ILIAS\Nolej\Notification\NolejNotificationPrefRepository;
+// use ILIAS\GlobalScreen\Scope\Notification\Provider\AbstractNotificationProvider;
+// use ILIAS\GlobalScreen\Scope\Notification\Provider\NotificationProvider;
 
 /**
  * Class NolejNotificationProvider
@@ -25,16 +25,16 @@ class NolejNotificationProvider extends AbstractNotificationPluginProvider
 	 */
 	public function getNotifications() : array
 	{
-		global $DIC;
-		$lng = $DIC->language();
-		$ui = $DIC->ui();
-		$user = $DIC->user();
-		$ctrl = $DIC->ctrl();
+		// global $DIC;
+		$lng = $this->dic->language();
+		$ui = $this->dic->ui();
+		$user = $this->dic->user();
+		$ctrl = $this->dic->ctrl();
 
 		// $lng->loadLanguageModule("badge");
 
 		$factory = $this->notification_factory;
-		$id = $this->id;
+		// $id = $this->id;
 
 		$new_activities = \ilNolejActivity::getNewCounter($user->getId());
 		if ($new_activities == 0) {
@@ -42,19 +42,25 @@ class NolejNotificationProvider extends AbstractNotificationPluginProvider
 		}
 
 		// Creating a Nolej Notification Item
-		$nolej_icon = $DIC->ui()->factory()->symbol()->icon()->standard("bdga", "NOLEJ")->withIsOutlined(true);
+		$nolej_icon = $ui->factory()->symbol()->icon()->standard("bdga", "NOLEJ")->withIsOutlined(true);
 		$nolej_title = $ui->factory()->link()->standard(
 			"Test notification", //$lng->txt("mm_badges"),
 			$ctrl->getLinkTargetByClass(["ilDashboardGUI"], "jumpToBadges")
 		);
 		$latest = new \ilDateTime(\ilNolejActivity::getLatestTimestamp($user->getId()), IL_CAL_UNIX);
-		$nolej_notification_item = $ui->factory()->item()->notification($nolej_title, $nolej_icon)
+		$nolej_notification_item = $ui
+			->factory()
+			->item()
+			->notification($nolej_title, $nolej_icon)
 			->withDescription(str_replace("%1", $new_activities, "New Nolej Activity"))
 			->withProperties([$lng->txt("time") => \ilDatePresentation::formatDate($latest)]);
 
-		$group = $factory->standardGroup($id('nolej_bucket_group'))->withTitle("Nolej activities")
+		$group = $factory
+			->standardGroup($this->id('nolej_bucket_group'))
+			->withTitle("Nolej activities")
 			->addNotification(
-				$factory->standard($id('nolej_bucket'))->withNotificationItem($nolej_notification_item)
+				$factory->standard($this->id('nolej_bucket'))
+				->withNotificationItem($nolej_notification_item)
 				// ->withClosedCallable(
 				// 	function () use ($user) {
 				// 		// Stuff we do, when the notification is closed
