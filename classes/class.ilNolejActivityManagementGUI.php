926d4c01efaf837b534491991457c9496982ce63
Vincenzo Padula <padvincenzo@gmail.com>
2023-06-05 14:59:18 +0200

    Remove duplicate form info

--- classes/class.ilNolejActivityManagementGUI.php
+++ classes/class.ilNolejActivityManagementGUI.php
@@ -202,7 +202,7 @@ class ilNolejActivityManagementGUI
 			// Available: web, audio, video, document, freetext.
 
 			$mediaWeb = new ilRadioOption($this->plugin->txt("prop_" . self::PROP_M_WEB), self::PROP_M_WEB);
-			$mediaWeb->setInfo($this->plugin->txt("prop_" . self::PROP_M_WEB . "_info"));
+			// $mediaWeb->setInfo($this->plugin->txt("prop_" . self::PROP_M_WEB . "_info"));
 			$mediaSource->addOption($mediaWeb);
 
 			$url = new ilUriInputGUI($this->plugin->txt("prop_" . self::PROP_M_URL), self::PROP_M_URL);
@@ -211,7 +211,7 @@ class ilNolejActivityManagementGUI
 			$mediaWeb->addSubItem($url);
 
 			$mediaMob = new ilRadioOption($this->plugin->txt("prop_" . self::PROP_M_MOB), self::PROP_M_MOB);
-			$mediaMob->setInfo($this->plugin->txt("prop_" . self::PROP_M_MOB . "_info"));
+			// $mediaMob->setInfo($this->plugin->txt("prop_" . self::PROP_M_MOB . "_info"));
 			$mediaSource->addOption($mediaMob);
 
 			$mob = new ilFileInputGUI($this->plugin->txt("prop_" . self::PROP_INPUT_MOB), self::PROP_INPUT_MOB);
@@ -220,7 +220,7 @@ class ilNolejActivityManagementGUI
 			$mediaMob->addSubItem($mob);
 
 			$mediaYT = new ilRadioOption($this->plugin->txt("prop_" . self::PROP_M_YT), self::PROP_M_YT);
-			$mediaYT->setInfo($this->plugin->txt("prop_" . self::PROP_M_YT . "_info"));
+			// $mediaYT->setInfo($this->plugin->txt("prop_" . self::PROP_M_YT . "_info"));
 			$mediaSource->addOption($mediaYT);
 
 			$url = new ilUriInputGUI($this->plugin->txt("prop_" . self::PROP_M_URL), self::PROP_INPUT_YT);
@@ -229,7 +229,7 @@ class ilNolejActivityManagementGUI
 			$mediaYT->addSubItem($url);
 
 			$mediaFile = new ilRadioOption($this->plugin->txt("prop_" . self::PROP_M_FILE), self::PROP_M_FILE);
-			$mediaFile->setInfo($this->plugin->txt("prop_" . self::PROP_M_FILE . "_info"));
+			// $mediaFile->setInfo($this->plugin->txt("prop_" . self::PROP_M_FILE . "_info"));
 			$mediaSource->addOption($mediaFile);
 
 			$file = new ilFileInputGUI($this->plugin->txt("prop_" . self::PROP_INPUT_FILE), self::PROP_INPUT_FILE);
@@ -243,7 +243,7 @@ class ilNolejActivityManagementGUI
 			$mediaFile->addSubItem($file);
 
 			$mediaText = new ilRadioOption($this->plugin->txt("prop_" . self::PROP_M_TEXT), self::PROP_M_TEXT);
-			$mediaText->setInfo($this->plugin->txt("prop_" . self::PROP_M_TEXT . "_info"));
+			// $mediaText->setInfo($this->plugin->txt("prop_" . self::PROP_M_TEXT . "_info"));
 			$mediaSource->addOption($mediaText);
 
 			$txt = new ilTextAreaInputGUI($this->plugin->txt("prop_" . self::PROP_M_TEXTAREA), self::PROP_M_TEXTAREA);
