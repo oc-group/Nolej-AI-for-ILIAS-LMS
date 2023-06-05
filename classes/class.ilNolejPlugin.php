cb14ff42e9b9d61eda1522b16898a1e0941a0139
Vincenzo Padula <padvincenzo@gmail.com>
2023-06-05 09:53:50 +0200

    Notification fix

--- classes/class.ilNolejPlugin.php
+++ classes/class.ilNolejPlugin.php
@@ -83,7 +83,7 @@ class ilNolejPlugin extends ilRepositoryObjectPlugin
 
 			// self::$pluginProviderCollection->setMetaBarProvider(self::helpMe()->metaBar());
 			self::$pluginProviderCollection->setMainBarProvider(new NolejMainBarProvider($DIC, $this));
-			self::$pluginProviderCollection->setNotificationProvider(new NolejNotificationProvider($DIC, $this));
+			// self::$pluginProviderCollection->setNotificationProvider(new NolejNotificationProvider($DIC, $this));
 		}
 
 		return self::$pluginProviderCollection;
