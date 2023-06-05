85a6967aa0766e44dcdb9f47816c06c3f2ddbffb
Vincenzo Padula <padvincenzo@gmail.com>
2023-06-05 10:06:37 +0200

    Notification fix

--- classes/NolejNotificationProvider.php
+++ classes/NolejNotificationProvider.php
@@ -24,88 +24,56 @@ class NolejNotificationProvider extends AbstractNotificationPluginProvider
 	public function getNotifications() : array
 	{
 		// // global $DIC;
-		// $lng = $this->dic->language();
-		// $ui = $this->dic->ui();
-		// $user = $this->dic->user();
-		// $ctrl = $this->dic->ctrl();
+		$lng = $this->dic->language();
+		$ui = $this->dic->ui();
+		$user = $this->dic->user();
+		$ctrl = $this->dic->ctrl();
 
 		// $lng->loadLanguageModule("badge");
 
-		// $factory = $this->notification_factory;
+		$factory = $this->notification_factory;
 		// $id = $this->id;
 
-		// $new_activities = \ilNolejActivity::getNewCounter($user->getId());
-		// if ($new_activities == 0) {
-		// 	return [];
-		// }
+		$new_activities = \ilNolejActivity::getNewCounter($user->getId());
+		if ($new_activities == 0) {
+			return [];
+		}
 
-		// // Creating a Nolej Notification Item
-		// $nolej_icon = $ui->factory()->symbol()->icon()->standard("bdga", "NOLEJ")->withIsOutlined(true);
-		// $nolej_title = $ui->factory()->link()->standard(
-		// 	"Test notification", //$lng->txt("mm_badges"),
-		// 	$ctrl->getLinkTargetByClass(["ilDashboardGUI"], "jumpToBadges")
-		// );
-		// $latest = new \ilDateTime(\ilNolejActivity::getLatestTimestamp($user->getId()), IL_CAL_UNIX);
-		// $nolej_notification_item = $ui
-		// 	->factory()
-		// 	->item()
-		// 	->notification($nolej_title, $nolej_icon)
-		// 	->withDescription("New Nolej Activity")
-		// 	->withProperties([$lng->txt("time") => \ilDatePresentation::formatDate($latest)]);
+		// Creating a Nolej Notification Item
+		$nolej_icon = $ui->factory()->symbol()->icon()->standard("bdga", "NOLEJ")->withIsOutlined(true);
+		$nolej_title = $ui->factory()->link()->standard(
+			"Test notification", //$lng->txt("mm_badges"),
+			$ctrl->getLinkTargetByClass(["ilDashboardGUI"], "jumpToBadges")
+		);
+		$latest = new \ilDateTime(\ilNolejActivity::getLatestTimestamp($user->getId()), IL_CAL_UNIX);
+		$nolej_notification_item = $ui
+			->factory()
+			->item()
+			->notification($nolej_title, $nolej_icon)
+			->withDescription("New Nolej Activity")
+			->withProperties([$lng->txt("time") => \ilDatePresentation::formatDate($latest)]);
 
-		// $group = $factory
-		// 	->standardGroup($this->id('nolej_bucket_group'))
-		// 	->withTitle("Nolej activities")
-		// 	->addNotification(
-		// 		$factory->standard($this->id('nolej_bucket'))
-		// 		->withNotificationItem($nolej_notification_item)
-		// 		// ->withClosedCallable(
-		// 		// 	function () use ($user) {
-		// 		// 		// Stuff we do, when the notification is closed
-		// 		// 		$noti_repo = new NolejNotificationPrefRepository($user);
-		// 		// 		$noti_repo->updateLastCheckedTimestamp();
-		// 		// 	}
-		// 		// )
-		// 		->withNewAmount($new_activities)
-		// 	)
-		// 	->withOpenedCallable(function () {
-		// 		// Stuff we do, when the notification is opened
-		// 	});
+		$group = $factory
+			->standardGroup($this->id('nolej_bucket_group'))
+			->withTitle("Nolej activities")
+			->addNotification(
+				$factory->standard($this->id('nolej_bucket'))
+				->withNotificationItem($nolej_notification_item)
+				// ->withClosedCallable(
+				// 	function () use ($user) {
+				// 		// Stuff we do, when the notification is closed
+				// 		$noti_repo = new NolejNotificationPrefRepository($user);
+				// 		$noti_repo->updateLastCheckedTimestamp();
+				// 	}
+				// )
+				->withNewAmount($new_activities)
+			)
+			->withOpenedCallable(function () {
+				// Stuff we do, when the notification is opened
+			});
 
-		// return [
-		// 	$group,
-		// ];
-
-		$factory = $this->globalScreen()->notifications()->factory();
-        $id = function (string $id) : IdentificationInterface {
-            return $this->if->identifier($id);
-        };
-
-		$new_mails = 2;
-		
-        //Creating a mail Notification Item
-        $mail_icon = $this->dic->ui()->factory()->symbol()->icon()->standard("mail","mail");
-        $mail_title = $this->dic->ui()->factory()->link()->standard("Inbox", 'ilias.php?baseClass=ilMailGUI');
-        $mail_notification_item = $this->dic->ui()->factory()->item()->notification($mail_title,$mail_icon)
-                                                   ->withDescription("You have $new_mails Mails.")
-                                                   ->withProperties(["Time" => "3 days ago"]);
-
-        $group = $factory->standardGroup($id('mail_bucket_group'))->withTitle($this->dic->language()->txt('mail'))
-            ->addNotification($factory->standard($id('mail_bucket'))->withNotificationItem($mail_notification_item)                                                      
-                ->withClosedCallable(
-                    function(){
-                        //@Todo: Memories, that those notifications have been closed.
-                        var_dump("Mail Notifications received closed event.");
-                    })
-                ->withNewAmount($new_mails)
-            )
-            ->withOpenedCallable(function(){
-                //@Todo: Memories, that those notifications have been seen.
-                var_dump("Mail Notifications received opened event.");
-            });
-
-        return [
-            $group,
-        ];
+		return [
+			$group,
+		];
 	}
 }
