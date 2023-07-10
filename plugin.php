e5cbbc1059bb8f65830ecbb4aef8c48c241bd74a
Vincenzo Padula <padvincenzo@gmail.com>
2023-07-10 22:30:38 +0200

    internal link input

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = "xnlj";
 
 // code version; must be changed for all code changes
-$version = "1.1.9";
+$version = "1.1.10";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
