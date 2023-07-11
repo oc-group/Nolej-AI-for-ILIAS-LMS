23d4b60ef0a92e378627fb5469e7a6b0311cfd56
Vincenzo Padula <padvincenzo@gmail.com>
2023-07-11 15:12:53 +0200

    Textarea rows

--- classes/class.ilNolejActivityManagementGUI.php
+++ classes/class.ilNolejActivityManagementGUI.php
@@ -781,7 +781,7 @@ class ilNolejActivityManagementGUI
 		 */
 		$txt = new ilTextAreaInputGUI($this->plugin->txt("prop_" . self::PROP_M_TEXT), self::PROP_M_TEXT);
 		$txt->setRequired(true);
-		$txt->setRows(20);
+		$txt->setRows(200);
 		if (ilObjAdvancedEditing::_getRichTextEditor() === "tinymce") {
 			$txt->setUseRte(true);
 			$txt->setRteTags([
