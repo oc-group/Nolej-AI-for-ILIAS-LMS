48f4f74a6c36552657fd5deb75cd0ef563657627
Vincenzo Padula <padvincenzo@gmail.com>
2023-07-11 12:13:39 +0200

    internal link input

--- classes/class.ilObjNolejGUI.php
+++ classes/class.ilObjNolejGUI.php
@@ -270,8 +270,6 @@ class ilObjNolejGUI extends ilObjectPluginGUI
 
 	protected function showContent() : void
 	{
-		$this->tabs->activateTab(self::TAB_CONTENT);
-
 		if ($this->object->status != "completed") {
 			$activityManagement = new ilNolejActivityManagementGUI($this);
 			$toolbar = new ilToolbarGUI();
@@ -285,6 +283,8 @@ class ilObjNolejGUI extends ilObjectPluginGUI
 			$this->tpl->setContent($toolbar->getHTML());
 			return;
 		}
+
+		$this->tabs->activateTab(self::TAB_CONTENT);
 
 		// if (!$this->object->isBound()) {
 		// 	// This module is not available yet
