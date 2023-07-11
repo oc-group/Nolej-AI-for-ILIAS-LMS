596243458a9f2af33824c35b1a72043cc079d9d8
Vincenzo Padula <padvincenzo@gmail.com>
2023-07-11 10:41:22 +0200

    internal link input

--- classes/class.ilNolejActivityManagementGUI.php
+++ classes/class.ilNolejActivityManagementGUI.php
@@ -6,7 +6,6 @@ include_once(ilNolejPlugin::PLUGIN_DIR . "/classes/class.ilNolejAPI.php");
  * @author Vincenzo Padula <vincenzo@oc-group.eu>
  * 
  * @ilCtrl_isCalledBy ilNolejActivityManagementGUI: ilObjNolejGUI
- * @ilCtrl_Calls ilNolejActivityManagementGUI: ilInternalLinkGUI
  */
 class ilNolejActivityManagementGUI
 {
@@ -91,7 +90,7 @@ class ilNolejActivityManagementGUI
 		global $tpl;
 		$cmd = $this->ctrl->getCmd();
 
-		$next_class = $this->ctrl->getNextClass($this);
+		$next_class = ""; //$this->ctrl->getNextClass($this);
         switch ($next_class) {
 			case "ilpropertyformgui":
 				// include_once './Services/Form/classes/class.ilPropertyFormGUI.php';
