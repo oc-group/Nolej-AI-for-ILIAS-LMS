5d8f2ee3acaf383025bdab0b6b4258dfff271b6b
Vincenzo Padula <padvincenzo@gmail.com>
2023-07-11 15:46:07 +0200

    Tabs back target

--- classes/class.ilNolejActivityManagementGUI.php
+++ classes/class.ilNolejActivityManagementGUI.php
@@ -235,7 +235,7 @@ class ilNolejActivityManagementGUI
 		global $tpl;
 
 		$this->tabs->setBackTarget(
-			$this->plugin->txt("cmd_go_back"),
+			$this->plugin->txt("cmd_back_to_content"),
 			$this->ctrl->getLinkTarget($this->gui_obj, "")
 		);
 
