d6fa7a3946b2359f37a149b5b2cb4d29ae8d306a
Vincenzo Padula <padvincenzo@gmail.com>
2023-10-30 18:42:06 +0100

    Implement checkUpdates

--- classes/class.ilNolejActivityManagementGUI.php
+++ classes/class.ilNolejActivityManagementGUI.php
@@ -286,7 +286,7 @@ class ilNolejActivityManagementGUI
             return;
         }
 
-        ilYuiUtil::initDom();
+        ilYuiUtil::initConnection();
         $tpl->addCss(ilNolejPlugin::PLUGIN_DIR . "/css/nolej.css");
         $tpl->addJavaScript(ilNolejPlugin::PLUGIN_DIR . "/js/nolej.js");
 
