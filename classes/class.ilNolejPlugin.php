938d9f6a6a87a22353f9d63f863abbaa54f07e9c
Vincenzo Padula <padvincenzo@gmail.com>
2023-11-01 23:03:51 +0100

    Update permission check

--- classes/class.ilNolejPlugin.php
+++ classes/class.ilNolejPlugin.php
@@ -41,9 +41,6 @@ class ilNolejPlugin extends ilRepositoryObjectPlugin
     protected static $pluginProviderCollection = null;
 
 
-    /** @var bool|null */
-    static $isAdmin = null;
-
     /**
      * Initialize plugin
      */
@@ -52,8 +49,6 @@ class ilNolejPlugin extends ilRepositoryObjectPlugin
         global $DIC;
 
         $this->provider_collection = $this->getPluginProviderCollection(); // Fix overflow
-        // $this->provider_collection = new PluginProviderCollection();
-        // $this->provider_collection->setNotificationProvider(new NolejNotificationProvider($DIC, $this));
     }
 
     /**
@@ -99,39 +94,6 @@ class ilNolejPlugin extends ilRepositoryObjectPlugin
         return $par_types;
     }
 
-    /**
-     * @return bool
-     */
-    public function isAdmin()
-    {
-        global $DIC, $ilUser;
-
-        if (self::$isAdmin != null) {
-            return self::$isAdmin;
-        }
-
-        if ($ilUser->isAnonymous()) {
-            return self::$isAdmin = false;
-        }
-
-        $rbacreview = $DIC['rbacreview'];
-        if (
-            $ilUser->getId() == SYSTEM_USER_ID ||
-            in_array(
-                SYSTEM_ROLE_ID,
-                $rbacreview->assignedRoles($ilUser->getId())
-            )
-        ) {
-            return self::$isAdmin = true;
-        }
-
-        ilUtil::sendFailure(
-            print_r($rbacreview->assignedRoles($ilUser->getId()), true),
-            true
-        );
-        return self::$isAdmin = false;
-    }
-
     protected function afterActivation()
     {
     }
@@ -161,24 +123,6 @@ class ilNolejPlugin extends ilRepositoryObjectPlugin
      */
     public function allowCopy()
     {
-        return false;
-    }
-
-    /** @return bool */
-    public function currentUserHasRole()
-    {
-        // global $ilUser;
-
-        // $user_roles = self::dic()->rbac()->review()->assignedGlobalRoles($ilUser->getId());
-        // return true;
-        // $config_roles = $this->config()->getValue(FormBuilder::KEY_ROLES);
-
-        // foreach ($user_roles as $user_role) {
-        // 	if (in_array($user_role, $config_roles)) {
-        // 		return true;
-        // 	}
-        // }
-
         return false;
     }
 
