bf25faa589210bcacf5b1b4c12c396c1e689ed54
Vincenzo Padula <padvincenzo@gmail.com>
2023-11-23 12:59:58 +0100

    Switch to staging URL

--- classes/class.ilNolejAPI.php
+++ classes/class.ilNolejAPI.php
@@ -16,8 +16,8 @@ require_once "./Customizing/global/plugins/Services/Repository/RepositoryObject/
  */
 class ilNolejAPI
 {
-    // public const API_URL = "https://api-staging.nolej.io";
-    public const API_URL = "https://api-dev.nolej.io";
+    public const API_URL = "https://api-staging.nolej.io";
+    // public const API_URL = "https://api-dev.nolej.io";
 
     /** @var string */
     private $apikey;
