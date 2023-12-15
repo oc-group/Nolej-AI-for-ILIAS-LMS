cea0617cea9bdfaf7e37b80193d52d03fb2fbaa5
Vincenzo Padula <padvincenzo@gmail.com>
2023-12-15 17:55:52 +0100

    Update LICENSE

--- plugin.php
+++ plugin.php
@@ -1,15 +1,21 @@
 <?php
-$id = "xnlj";
 
-// code version; must be changed for all code changes
+/**
+ * This file is part of Nolej Repository Object Plugin for ILIAS,
+ * developed by OC Open Consulting to integrate ILIAS with Nolej
+ * software by Neuronys.
+ *
+ * @author Vincenzo Padula <vincenzo@oc-group.eu>
+ * @copyright 2023 OC Open Consulting SB Srl
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$id = "xnlj";
 $version = "1.3.0";
 
-// ilias min and max version; must always reflect the versions that should
-// run with the plugin
 $ilias_min_version = "6.0";
 $ilias_max_version = "7.999";
 
-// optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "OC Open Consulting";
 $responsible_mail = "plugins@oc-group.eu";
 
